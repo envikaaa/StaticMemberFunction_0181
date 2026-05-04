ba3ce96f6ab13c2de0bf87fcde8199743894fce2
envikaaa <envikapriyatna31@gmail.com>
2026-05-04 16:15:22 +0700

    menambahkan class

--- constructorberparameter.cpp
+++ constructorberparameter.cpp
@@ -8,6 +8,13 @@ public:
 };
 
 //definisi constructor dengan parameter
-mahasiswa::mahasiswa(int nim, string nama); {
-    cout << 
+Mahasiswa::mahasiswa(int nim, string nama); {
+    cout << "Constructor Dengan Parameter Terpanggil" << endl;
+    cout << "NIM : " << nim << endl;
+    cout << "Nama : " << nama << endl;
+}
+
+int main(){
+    mahasiswa mhs(12345, "Pascal"); //memanggil constructor dengan parameter
+    return 0;
 }
