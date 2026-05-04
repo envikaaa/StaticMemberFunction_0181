1a15b0d9800172daeacd4f3014e5d7cac34fe6af
envikaaa <envikapriyatna31@gmail.com>
2026-05-04 15:35:37 +0700

    memanggil prosedur

--- staticmember.cpp
+++ staticmember.cpp
@@ -37,4 +37,10 @@ int main() {
 
     //membuat object dan memberi nilai
     Mahasiswa mhs1("Lia Kurnia");
+    Mahasiswa mhs2("Asroni");
+    Mahasiswa mhs3("Andi Kurniawan");
+    Mahasiswa mhs4("Joko Purbo");
+
+    //memanggil prosedur printAll()
+    mhs1.printAll();
 }
