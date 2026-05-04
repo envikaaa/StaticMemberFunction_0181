bcf41f53ebf79f255eef51aba55248d6af8865c1
envikaaa <envikapriyatna31@gmail.com>
2026-05-04 16:00:15 +0700

    menampilkan nim terakhir yg diproses

--- staticfunction.cpp
+++ staticfunction.cpp
@@ -11,16 +11,16 @@ public:
     string nama;
 
     void setID();
-    voidprintAll();
+    void printAll();
 
     //pembuatan static function
     static void setNim(int pnim) {
         nim = pnim;
     };
 
-    static int getNim() [
+    static int getNim() {
         return nim;
-    ]
+    }
 
     Mahasiswa(string pnama) :nama(pnama) {
         setID();
@@ -34,3 +34,31 @@ void Mahasiswa::setID() {
     id = ++nim;
 }
 
+void Mahasiswa::printAll() {
+    cout << "ID = " << id << endl;
+    cout << "Nama = " << nama << endl;
+    cout << endl;
+}
+
+int main(){
+
+    //pembuatan object dan pemberian nilai
+    Mahasiswa mhs1("Sri Dadi");
+    Mahasiswa mhs2("Budi Jatmiko");
+    //memberi nilai pada setNim() untuk merubah nilai NIM
+    Mahasiswa::setNim(9);
+    Mahasiswa mhs3("Andi Janu");
+    Mahasiswa mhs4("Joko Wahono");
+
+    //memanggil prosedur printAll()
+    mhs1.printAll();
+    mhs2.printAll();
+    mhs3.printAll();
+    mhs4.printAll();
+
+    //menampilkan NIM terakhir yang diproses
+    cout << "Akses diluar object = " << Mahasiswa::getNim() << endl;
+    system("Pause");
+
+    return 0;
+}
