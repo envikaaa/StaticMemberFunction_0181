3592a7108c7215e2033ee6ef3d448ec92bd06710
envikaaa <envikapriyatna31@gmail.com>
2026-05-04 16:27:28 +0700

    menambahkan int main

--- chainfunctional.cpp
+++ chainfunctional.cpp
@@ -3,5 +3,19 @@ using namespace std;
 class buku {
     string judul;
 
-    
+    public:
+    buku &setJudul(string judul){
+        this->judul = judul;
+        return *this; //chain function
+    }
+    string getJudul(){
+        return this->judul;
+    }
+};
+
+int main()
+{
+    buku bukunya;
+    cout << bukunya.setJudul("Matematika").getJudul();
+    return 0;
 }
